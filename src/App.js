5da8f1b5273872c3d945c58751b76fc814a270a3
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2022-12-19 12:38:44 +0200

    last version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import React from "react";
+import Cell from "./Cell";
+
+
 
 class App extends React.Component {
-
     state = {
-        player1: 1,
-        player2: 2,
+        player1:1,
+        player2:2,
         currentPlayer: null,
         board: [],
         gameOver: false,
-    };
+        write: ''
+    }
 
     initBoard() {
         let board = [];
@@ -18,70 +21,192 @@ class App extends React.Component {
             for (let c = 0; c < 7; c++) { row.push(null) }
             board.push(row);
         }
+
         this.setState({
             board,
             currentPlayer: this.state.player1,
-            gameOver: false,
+            write: 'Red player turn',
+            gameOver: false
         });
     }
 
-    togglePlayer() {
-        return (this.state.currentPlayer === this.state.player1) ? this.state.player2 : this.state.player1;
-    }
-
-    play=(c)=> {
-        alert("hi")
-        let board = this.state.board;
-        for (let r = 5; r >= 0; r--) {
-            if (!board[r][c]) {
-                board[r][c] = 1;
-                break;
+    cellClicked = (row,cell) => {
+        let currentColor = this.state.board;
+        if (!this.state.gameOver) {
+            for (let i = 5; i >= 0; i--) {
+                if (currentColor[i][cell] === null) {
+                    currentColor[i][cell] = this.state.currentPlayer;
+                    this.setState({
+                        board: currentColor,
+                        currentPlayer: this.switchPlayer(),
+                    })
+                    this.checkAll(row, cell);
+                    break
+                }
             }
         }
-        this.setState({ board , currentPlayer: this.togglePlayer() });
     }
+
+
+    checkAll () {
+        if (this.checkDiagonalRight() || this.checkHorizontal()
+            || this.checkVertical() || this.checkDiagonalLeft()){
+            this.setState({
+                gameOver: true
+            })
+            if(this.state.currentPlayer === 1){
+                this.setState({
+                    write: "Red player wins!"
+                })
+            }
+            else {
+                this.setState({
+                    write: "Yellow player wins!"
+                })
+            }
+        }
+        else {
+            if(!this.state.board.isEmpty){
+                this.setState(
+                    {
+                        write:"it's a draw"
+                    }
+                )
+            }
+        }
+    }
+
+
+    checkHorizontal() {
+        let board = this.state.board
+        let win = false
+        for (let r = 0; r < 6; r++) {
+            for (let c = 0; c < 4; c++) {
+                if (board[r][c]) {
+                    if (board[r][c] === board[r][c + 1] &&
+                        board[r][c] === board[r][c + 2] &&
+                        board[r][c] === board[r][c + 3]) {
+                        win = true
+                        return win
+                    }
+                }
+            }
+        }
+    }
+
+
+    checkVertical() {
+        let board = this.state.board
+        let win = false;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (board[i][j]) {
+                    if (board[i][j] === board[i + 1][j]
+                        && board[i][j] === board[i + 2][j]
+                        && board[i][j] === board[i + 3][j]) {
+                        win = true
+                        return win
+                    }
+                }
+            }
+        }
+    }
+
+
+    checkDiagonalRight(){
+        let board = this.state.board
+        let win = false
+        for (let x = 3; x < 6 ; x++) {
+            for (let y = 0; y < 4; y++) {
+                if (board[x][y]){
+                    if (board[x][y] === board[x-1][y+1] &&
+                        board[x][y] === board[x-2][y+2] &&
+                        board[x][y] === board[x-3][y+3]){
+                        win = true
+                        return win
+                    }
+                }
+            }
+        }
+    }
+
+
+    checkDiagonalLeft(){
+        let board = this.state.board
+        let win = false
+        for (let r = 3; r < 6 ; r++) {
+            for (let p = 3; p < 7; p++) {
+                if (board[r][p]){
+                    if (board[r][p] === board[r-1][p+1] &&
+                        board[r][p] === board[r-2][p+2] &&
+                        board[r][p] === board[r-3][p+3]){
+                        if (board[r][p] === board[r-1][p-1] &&
+                            board[r][p] === board[r-2][p-2] &&
+                            board[r][p] === board[r-3][p-3]){
+                            win = true
+                            return win
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    switchPlayer () {
+
+        if(this.state.currentPlayer === this.state.player1) {
+            this.setState({
+                write: 'Yellow player turn'
+            })
+            return(this.state.player2)
+        } else {
+            this.setState({
+                currentPlayer: this.state.player1,
+                write: 'Red player turn'
+            })
+            return(this.state.player1)
+        }
+    }
+
 
     componentWillMount() {
         this.initBoard();
     }
 
-    Cell = ({ value, columnIndex}) => {
-        let color = 'white';
-        if (value === 1) {
-            color = 'red';
-        } else if (value === 2) {
-            color = 'yellow';
-        }
-        return (
-            <td>
-                <div className="cell" onClick={() => {this.play(columnIndex)}}>
-                    <div className={color}></div>
-                </div>
-            </td>
-        );
-    };
-
-    Row = ({ row, play }) => {
-        return (
-            <tr>
-                {row.map((cell, i) => this.Cell = () =>  key={i} value={cell} columnIndex={i} play={play}}
-            </tr>
-        );
-    };
-
-
 
     render() {
-        debugger
         return (
-            <div>
-                <h1>Connect 4</h1>
+            <div className={"App"}>
+                <h1>Connect4</h1>
+                <hr/>
+                <h2><div className={"write"} >{this.state.write}</div></h2>
+                <div className={"tb"}>
+                    <table>
+                        {
+                            this.state.board.map((row, rowIndex) => {
+                                return(
+                                    <tr>
+                                        {
+                                            row.map((cell, cellIndex) =>{
+                                                return(
+                                                    <Cell
+                                                        value = {this.state.board[rowIndex][cellIndex]}
+                                                        cellClicked = {this.cellClicked}
+                                                        row = {rowIndex}
+                                                        cell = {cellIndex}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </tr>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+                <br/><hr/><br/>
                 <div className="button" onClick={() => {this.initBoard()}}>New Game</div>
-                <table>
-                    <tbody>
-                        {this.state.board.map((row, i) => (<Row key={i} row={row} play={this.play} />))}
-                    </tbody>
-                </table>
             </div>
         );
     }
---
+++ src/Cell.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function Cell(props){
+    let bg = 'white';
+    if (props.value === 1) {
+        bg = 'red';
+    } else if (props.value === 2) {
+        bg = 'yellow';
+    }
+    return(
+        <td style = {{backgroundColor: bg}} onClick={() =>
+            props.cellClicked(props.row, props.cell)}>
+
+        </td>
+    )
+}
+
+export default Cell;
